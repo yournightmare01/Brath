e5d99869cacb0a64cc795554d6df757e23814d23
yournightmare01 <74112655+yournightmare01@users.noreply.github.com>
2023-01-17 20:41:20 +0100

    Delete src directory

--- src/common/types.ts
+++
@@ -1,8 +0,0 @@
-import { MouseEventHandler } from 'react';
-
-export type SvgProps = {
-  className: string;
-  onClick?: MouseEventHandler<SVGSVGElement>;
-};
-
-export type InputType = 'text' | 'password' | 'email';
